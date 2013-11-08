aa1ab7533e4611bf109e40303da4714a4e7a596f
AQfaruqi@gmail.com <AQfaruqi@gmail.com@02d0fb59-8236-cbf5-5a60-e3f1517b06b8>
2013-11-08 16:51:57 +0000

    original

---
+++ main.s
@@ -0,0 +1,30 @@
+.section    .init
+.globl     _start
+
+_start:
+    b       main
+    
+.section .text
+
+main:
+	bl EnableJTAG
+	
+	bl	UART
+	
+	bl	InitFrameBuffer
+	mov	r0,	#10
+	mov	r1,	#10
+	mov	r2,	#0x000000
+	bl	drawPixel
+	    
+Done:
+	b Done
+
+.section .data
+.align 4
+
+
+
+
+
+
