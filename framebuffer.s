19373394fc2926a70c659a4dc7d3a46a34c21fe4
rnhadfie@gmail.com <rnhadfie@gmail.com@02d0fb59-8236-cbf5-5a60-e3f1517b06b8>
2013-11-10 00:41:08 +0000

    drawPixel will now work without error

--- framebuffer.s
+++ framebuffer.s
@@ -2,107 +2,108 @@
 .align 12
 
 FrameBufferInfo:
-	.int	1024		// 0 - Width
-	.int	768			// 4 - Height
-	.int	1024		// 8 - vWidth
-	.int	768			// 12 - vHeight
-	.int	0			// 16 - Pitch (set by GPU)
-	.int	16			// 20 - Bit Depth (bpp)
-	.int	0			// 24 - X virtual offset
-	.int	0			// 28 - Y virtual offset
-	.int	0			// 32 - Framebuffer Pointer (set by GPU)
-	.int	0			// 36 - Framebuffer Size (set by GPU)
+    .int    1024        // 0 - Width
+    .int    768            // 4 - Height
+    .int    1024        // 8 - vWidth
+    .int    768            // 12 - vHeight
+    .int    0            // 16 - Pitch (set by GPU)
+    .int    16            // 20 - Bit Depth (bpp)
+    .int    0            // 24 - X virtual offset
+    .int    0            // 28 - Y virtual offset
+    .int    0            // 32 - Framebuffer Pointer (set by GPU)
+    .int    0            // 36 - Framebuffer Size (set by GPU)
 
 .align 4
 .globl FrameBufferPointer
 FrameBufferPointer:
-	.int	0
+    .int    0
+
+colour:
+    .int    0xffffff
 
 .section .text
 
 /* Initialize the FrameBuffer
  * Return:
- *	r0 - message
+ *    r0 - message
  */
 
 .globl InitFrameBuffer
 InitFrameBuffer:
-	push	{r4, lr}
-	infoAdr	.req	r4
-	result	.req	r0	
+    push    {r4, lr}
+    infoAdr    .req    r4
+    result    .req    r0    
 
-	ldr		infoAdr, =FrameBufferInfo
+    ldr        infoAdr, =FrameBufferInfo
 
-	mov		r0, infoAdr
-	mov		r1, #1
-	bl		MailboxWrite			// write FrameBufferInfo address to channel 1 (FrameBuffer) on the mailbox interface
+    mov        r0, infoAdr
+    mov        r1, #1
+    bl        MailboxWrite            // write FrameBufferInfo address to channel 1 (FrameBuffer) on the mailbox interface
 
-	mov		r0, #1
-	bl		MailboxRead				// read from channel 1 (FrameBuffer) on the mailbox interface
+    mov        r0, #1
+    bl        MailboxRead                // read from channel 1 (FrameBuffer) on the mailbox interface
 
-	teq		result, #0
-	movne	result, #0
-	popne	{r4, pc}
+    teq        result, #0
+    movne    result, #0
+    popne    {r4, pc}
 
 pointerWait$:
-	ldr		result, [infoAdr, #32]	// load the value of the framebuffer pointer
-	teq		result, #0
-	beq		pointerWait$			// loop while the pointer is still 0
+    ldr        result, [infoAdr, #32]    // load the value of the framebuffer pointer
+    teq        result, #0
+    beq        pointerWait$            // loop while the pointer is still 0
 
-	ldr		r1,		=FrameBufferPointer
-	str		result, [r1]			// store the framebuffer pointer
+    ldr        r1,        =FrameBufferPointer
+    str        result, [r1]            // store the framebuffer pointer
 
-	mov		result, infoAdr
-	pop		{r4, pc}
+    mov        result, infoAdr
+    pop        {r4, pc}
 
-	.unreq	result
-	.unreq	infoAdr
+    .unreq    result
+    .unreq    infoAdr
 
 
-//r4 = x
-//r5 = y
-//r6 = colour
+//r0 = x
+//r1 = y
+//r2 = colour
 .global drawPixel
 
 drawPixel:
- 
-    push    {r4,r5,r6, lr}
+    
     
     /* r0 is x, r1 is y and r3 is addr*/
     
     x    .req    r0
     y    .req    r1
     addr    .req    r3
-    mov    x,    r4
-    mov    y,    r5
-    mov    r2,    r6   //r2 is colour
+    
     ldr    addr,    =FrameBufferInfo
-    //checks if the x and y  are within the frame buffer 
+    
     height    .req    r4
     ldr    height,    [addr,    #4]
     sub    height,    #1
     cmp    y,    height
-    bhi    popPixel
+    movhi    pc,    lr
     .unreq    height    
 
     width    .req    r4
     ldr    width,    [addr,    #0]
     sub    width,    #1
     cmp    x,    width
-    bhi    popPixel
+    movhi    pc,    lr
     
-    //get frame pointer
+    
     ldr    addr,    =FrameBufferPointer
     ldr    addr,    [addr]
     add    width,    #1
-    mla    x,    y,width, x  //x=(y*1024)+x
+    mla    x,    y,width, x
     
     .unreq    y
 
-    add    addr,    x, lsl #1  //add x*2 to frame buffer pointer
+    add    addr,    x, lsl #1
     .unreq    x
-    //stores colour in address
+    
     strh    r2,    [addr]
     .unreq    addr    
-    poppixel:
-    pop    {r4,r5,r6, pc}
+    popPixel:
+    mov    pc,    lr
+
