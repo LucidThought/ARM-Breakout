d760f8502085c8d9524fd77d76f5a9f8070d65a6
rnhadfie@gmail.com <rnhadfie@gmail.com@02d0fb59-8236-cbf5-5a60-e3f1517b06b8>
2013-11-10 00:41:43 +0000

    2 working methods

--- draw.s
+++ draw.s
@@ -1,59 +1,69 @@
-.section .text
-.global drawHorizontalLine
-/*r4 =start point(x), r5 = start point(y) , r6= colour, r7= length*/
-drawHorizontalLine:
-	push	{r4, r5, r6, r7, lr}
-	x 	.req 	r0	
-	y 	.req	r1	
-	length	.req	r3	
-	mov	x,	r4
-	mov	y,	r5
-	mov	length,	r7
-	/*Check if the x and y values are in with in the range of the Frame Buffer*/
-	
-	mov	r4,	#1
-	//draws the line
-	hDrawLoop:
-	cmp	r4,	length //checks when to stop drawing pixels
-	bhi	doneH      
-	mov	r2,	r6
-	bl	drawPixel    //draws pixel
-	add	x,	#1      //moves x to the next address
-  add  r4, #1      //increments count
-	
-	doneH:
-	.unreq	x
-	.unreq	y
-	.unreq	length
-	pop {r4, r5, r6, r7, pc}
 
-.global	drawVerticalLine
-drawVerticalLine:
-	push	{r4, r5, r6, r7, lr}
-	x 	.req 	r4	
-	y 	.req	r5 
-	length	.req	r7	
-  mov  r3, 0
-	/*Draws vertical line*/
-	vDrawLoop:
-	add	r3,	#1
-	bl	drawPixel
-	add	x,	#1024
-	cmp	r3,	length
-	bhi	vDrawLoop
-	doneV:
-   
-	.unreq	x
-	.unreq	y
-	.unreq	length
-	pop {r4, r5, r6, r7, pc}
+.global drawHLine
+/*r4 =start point(x), r5 = start point(y) , r7= length*/
+drawHLine:
+    push    {lr}
+    x     .req     r0    
+    y     .req    r1
+    colour    .req    r2    
+    length    .req    r3    
+    
+    /*Check if the x and y values are in with in the range of the Frame Buffer*/
+    
+    mov    r8,    #0
+    mov    r5,    length
+    //draws the line
+    hDrawLoop:
+    mov    r7,    x
+    
 
+    cmp    r8,    length
+    bhi    doneH
+    
+    bl    drawPixel
+    mov    x,    r7
+    add    x,    #1
+    mov    length,    r5
+    add    r8,    #1
+    
+    b hDrawLoop
+    doneH:
+    .unreq    x
+    .unreq    y
+    .unreq    length
+    pop    {pc}
 
-.section	.data
-colour:
-	.int	0xffffff	
-width:
-	.int	1024
-height:
-	.int	724
-	
+.global drawVLine
+/*r4 =start point(x), r5 = start point(y) , r7= length*/
+drawVLine:
+    push    {lr}
+    x     .req     r0    
+    y     .req    r1
+    colour    .req    r2    
+    length    .req    r3    
+    
+    /*Check if the x and y values are in with in the range of the Frame Buffer*/
+    
+    mov    r8,    #0
+    mov    r5,    length
+    //draws the line
+    VDrawLoop:
+    mov    r7,    x
+    
+
+    cmp    r8,    length
+    bhi    doneV
+    
+    bl    drawPixel
+    mov    x,    r7
+    add    y,    #1
+    mov    length,    r5
+    add    r8,    #1
+    
+    b VDrawLoop
+    doneV:
+    .unreq    x
+    .unreq    y
+    .unreq    length
+    pop    {pc}
+
